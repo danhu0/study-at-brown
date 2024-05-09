530b954454911b1c325a45bf291eaa4ebd8fe11a
Daniel Hu <daniel_hu4@brown.edu>
2024-05-09 15:03:38 -0400

    test: added  more tests

--- server/src/test/java/edu/brown/cs/student/TestHandlersNew.java
+++ server/src/test/java/edu/brown/cs/student/TestHandlersNew.java
@@ -14,6 +14,7 @@ import java.net.URL;
 import java.util.logging.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.apache.hadoop.shaded.org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -138,13 +139,41 @@ public class TestHandlersNew {
                     tryRequest("get-recs?uid=mock-user&num-recs=3").getInputStream()))
             .readLine(),
         "{\"response_type\":\"success\",\"best_spots\":[{\"id\":\"2\",\"title\":\"Main Green\",\"building\":\"Outside\",\"Description\":\"\",\"natural_light_level\":\"4\",\"noise_level\":\"2\",\"outlet_availability\":\"0\",\"room_size\":\"2\",\"private\":\"0\",\"food\":\"0\",\"view\":\"0\",\"home\":\"1\",\"latitude\":\"41.82623\",\"longitude\":\"-71.40338\",\"hours_open\":\"n/a\",\"hours_close\":\"n/a\",\"days_open\":\"1111111\",\"hours\":\"\",\"study_room\":\"0\",\"google_link \":\"https://www.google.com/maps/place/John+D.+Rockefeller,+Jr.+Library/@41.8257007,-71.4051436,15z/data=!4m6!3m5!1s0x89e44446ec7c0d51:0x2fde92d441a35057!8m2!3d41.8257007!4d-71.4051436!16s%2Fm%2F047mq5j?entry=ttu\",\"campus_position\":\"south\"},{\"id\":\"5\",\"title\":\"Kassar FOX\",\"building\":\"Kassar\",\"Description\":\"\",\"natural_light_level\":\"0\",\"noise_level\":\"1\",\"outlet_availability\":\"1\",\"room_size\":\"2\",\"private\":\"1\",\"food\":\"0\",\"view\":\"0\",\"home\":\"0\",\"latitude\":\"41.82527\",\"longitude\":\"-71.40003\",\"hours_open\":\"\",\"hours_close\":\"17:00\",\"days_open\":\"0111110\",\"hours\":\"\",\"study_room\":\"0\",\"google_link \":\"https://www.google.com/maps/place/John+D.+Rockefeller,+Jr.+Library/@41.8257007,-71.4051436,15z/data=!4m6!3m5!1s0x89e44446ec7c0d51:0x2fde92d441a35057!8m2!3d41.8257007!4d-71.4051436!16s%2Fm%2F047mq5j?entry=ttu\",\"campus_position\":\"south\"},{\"id\":\"3\",\"title\":\"134C\",\"building\":\"The Rock\",\"Description\":\"\",\"natural_light_level\":\"1\",\"noise_level\":\"0\",\"outlet_availability\":\"2\",\"room_size\":\"0\",\"private\":\"2\",\"food\":\"1\",\"view\":\"0\",\"home\":\"0\",\"latitude\":\"41.82568\",\"longitude\":\"-71.40502\",\"hours_open\":\"8:00\",\"hours_close\":\"2:00 AM\",\"days_open\":\"1111111\",\"hours\":\"\",\"study_room\":\"1\",\"google_link \":\"https://www.google.com/maps/place/John+D.+Rockefeller,+Jr.+Library/@41.8257007,-71.4051436,15z/data=!4m6!3m5!1s0x89e44446ec7c0d51:0x2fde92d441a35057!8m2!3d41.8257007!4d-71.4051436!16s%2Fm%2F047mq5j?entry=ttu\",\"campus_position\":\"south\"}]}");
+
+    assertTrue(
+        new BufferedReader(
+                new InputStreamReader(
+                    tryRequest("get-recs?uid=mock-user&num-recs=1&natural_light_level=4")
+                        .getInputStream()))
+            .readLine()
+            .contains("Main Green"));
   }
 
   @Test
   public void testGetHot() throws IOException {
-    assertTrue(
-        new BufferedReader(new InputStreamReader(tryRequest("get-hot").getInputStream()))
-            .readLine()
-            .contains("success"));
+    int count =
+        StringUtils.countMatches(
+            (new BufferedReader(new InputStreamReader(tryRequest("get-hot").getInputStream()))
+                .readLine()),
+            "id");
+    assertEquals(5, count);
+    count =
+        StringUtils.countMatches(
+            (new BufferedReader(new InputStreamReader(tryRequest("get-hot").getInputStream()))
+                .readLine()),
+            "id");
+    assertEquals(4, count);
+    count =
+        StringUtils.countMatches(
+            (new BufferedReader(new InputStreamReader(tryRequest("get-hot").getInputStream()))
+                .readLine()),
+            "id");
+    assertEquals(5, count);
+    count =
+        StringUtils.countMatches(
+            (new BufferedReader(new InputStreamReader(tryRequest("get-hot").getInputStream()))
+                .readLine()),
+            "id");
+    assertEquals(5, count);
   }
 }
